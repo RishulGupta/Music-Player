80b0619444cc0bb12bc79cc39c91f0b25050b0e9
Rishul Gupta <rishulguptaofficial@gmail.com>
2025-07-30 12:50:24 +0530

    first commit

---
+++ app/src/main/java/com/uilover/project240/SongAdapter.java
@@ -0,0 +1,81 @@
+package com.uilover.project240;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.uilover.project240.databinding.ItemSongBinding;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewholder> {
+    private final List<Song> songs;
+    private final OnItemClickListerner listerner;
+
+    public interface OnItemClickListerner {
+        void onItemClick(int position);
+    }
+
+    public SongAdapter(List<Song> songs, OnItemClickListerner listerner) {
+        this.songs = songs;
+        this.listerner = listerner;
+    }
+
+    @NonNull
+    @Override
+    public SongAdapter.SongViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemSongBinding binding = ItemSongBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false);
+        return new SongViewholder(binding, listerner);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongAdapter.SongViewholder holder, int position) {
+        Song song = songs.get(position);
+        holder.binding.textTitle.setText(song.title);
+        holder.binding.textArtist.setText(song.artist);
+
+        Uri albumArtUri = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), song.albumId);
+
+        Glide.with(holder.binding.getRoot().getContext())
+                .load(albumArtUri)
+                .circleCrop()
+                .placeholder(R.drawable.ic_music_note_24)
+                .error(R.drawable.ic_music_note_24)
+                .into(holder.binding.imageAlbumArt);
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public class SongViewholder extends RecyclerView.ViewHolder {
+        final ItemSongBinding binding;
+        final OnItemClickListerner listener;
+
+        public SongViewholder(ItemSongBinding binding, OnItemClickListerner listerner) {
+
+            super(binding.getRoot());
+            this.binding = binding;
+            this.listener = listerner;
+            binding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listerner != null) {
+                        int pos = getAdapterPosition();
+                        if (pos != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(pos);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/uilover/project240/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.uilover.project240;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.uilover.project240.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.startBtn.setOnClickListener(view ->
+                startActivity(new Intent(SplashActivity.this, MainActivity.class)));
+    }
+}
